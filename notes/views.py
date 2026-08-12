831e410924e94271384fdb7922f1c67f713743aa
purabiyapalakben <purabiyapalakben@gmail.com>
2026-08-12 18:49:46 +0530

    Added logout_view function

--- notes/views.py
+++ notes/views.py
@@ -14,7 +14,8 @@ def register_view(request):
     return render(request, 'register.html')
 def login_view(request):
     return render(request, 'login.html')
-
+def logout_view(request):
+    return render(request, 'logout.html')
 
 # 2. Response Viewa
 def generate_response(request):

ccb921353b40115258e7f972f999ad37ecba2de1
purabiyapalakben <purabiyapalakben@gmail.com>
2026-08-12 19:26:10 +0530

    Fix login URL

--- notes/urls.py
+++ notes/urls.py
@@ -2,29 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        "",
-        views.home,
-        name="home"
-    ),
+    path("", views.home, name="home"),
 
-    path(
-        "register/",
-        views.register_view,
-        name="register"
-    ),
+    path("register/", views.register_view, name="register"),
 
-    path(
-        "login/",
-        views.login_view,
-        name="login_view"
-    ),
+    path("login/", views.login_view, name="login"),
 
-    path(
-        "logout/",
-        views.logout_view,
-        name="logout_view"
-    ),
+    path("logout/", views.logout_view, name="logout"),
 
     path(
         "generate-response/",
